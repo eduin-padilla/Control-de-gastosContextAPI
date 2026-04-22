a6025d593ddd2b61b5d9d5f77a365830a8906969
eduin-padilla <edwinpadillaorozco1@gmail.com>
2026-04-21 20:46:21 -0500

    haciendo tematica la rueda con porcentage de gastos

--- src/components/BudgetTraker.tsx
+++ src/components/BudgetTraker.tsx
@@ -1,33 +1,50 @@
 import AmountDisplay from "./AmountDisplay";
+import {useBudget} from "../hooks/useBudget";
+import  {  CircularProgressbar, buildStyles} from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function BudgetTraker() {
+    const {state, totalExpense, remainingBudget, dispatch} = useBudget()
+
+    const  percentage = +((totalExpense / state.budget) * 100).toFixed(2)
+
+
     return (
         <>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5 border-gray-300 rounded-lg p-5 shadow-lg ">
                 <div className="flex justify-center">
-                    <img className="" src="/public/grafico.jpg" alt="grafico" />
+                    <CircularProgressbar
+                    value={percentage}
+                    styles={buildStyles({
+                        pathColor: percentage === 100  ? '#DC2626' : '#3b82f6',
+                        trailColor: '#f5f5f5',
+                        textSize: 8,
+                        textColor: percentage === 100  ? '#DC2626' : '#3b82f6'
+                    })}
+                    text={`${percentage}%Gastado`}
+                    />
                 </div>
-                <div className="flex flex-col justify-center items-center">
+                <div className="flex flex-col justify-center items-center gap-5">
                     <button 
                     type="button"
-                    
+                    onClick={() => dispatch({type: 'reset-app'})}
                     className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg hover:bg-pink-700 transition-colors cursor-pointer">
                         Actualizar Presupuesto
                     </button>
 
                     <AmountDisplay 
                     label = "presupuesto"
-                    amount={2000}
+                    amount={state.budget}
+                    />
+
+                    <AmountDisplay
+                    label = "disponible"
+                    amount={remainingBudget}
                     />
 
                     <AmountDisplay
                     label = "gastado"
-                    amount={900}
-                    />
-
-                    <AmountDisplay
-                    label = "total"
-                    amount={100}
+                    amount={totalExpense}
                     />
                 </div>
             </div>
--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,9 +1,11 @@
-import {createContext, useReducer, type Dispatch, type PropsWithChildren} from "react"
+import {createContext, useMemo, useReducer, type Dispatch, type PropsWithChildren} from "react"
 import { budgetReducer, initialState, type BudgetActions, type BudgetState} from "../reducers/BudgetReducer"
 
 type BudgetContextProps = {
     state: BudgetState
     dispatch:  Dispatch<BudgetActions>
+    totalExpense: number
+    remainingBudget: number
 }
 
 
@@ -14,9 +16,13 @@ export const BudgetContext = createContext<BudgetContextProps>(null!)
 export const BudgetProvider = ({children}: PropsWithChildren) => {
     const [state, dispatch] = useReducer(budgetReducer, initialState)
 
+    const totalExpense  =  useMemo(() => state.expense.reduce((total, expense) => total + expense.amount, 0) , [state.expense])
+    
+        const remainingBudget =  state.budget - totalExpense
+
     return (
         <BudgetContext.Provider 
-        value={{state, dispatch}}
+        value={{state, dispatch, totalExpense, remainingBudget}}
         >
             {children}
         </BudgetContext.Provider>
